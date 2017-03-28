fc4c52a05001d5e9f40619d09c858b30123fe1ed
markruoleili@gmail.com <mark@Marks-MacBook-Pro.local>
2017-03-27 19:17:59 -0700

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# config/initializers/high_voltage.rb
+HighVoltage.configure do |config|
+  config.home_page = 'home'
 end
