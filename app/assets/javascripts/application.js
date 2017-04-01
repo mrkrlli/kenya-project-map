79b0208af340b96a5935766ff3c05d1c975d840b
Mark Li <mark@philosophie.is>
2017-04-01 15:54:42 -0700

    Add map markers for each project from json data (#2)
    - Also, refocus map to display Kenya location.
    
    
    * Add map markers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,12 +16,39 @@
 //= require_tree .
 
 $(function(){
-  var mymap = L.map('main-map').setView([51.505, -0.09], 13);
+  // load data from json file
+  var projectData;
+
+  $.ajax({
+    url: '/projects-data.json',
+    dataType: 'json',
+    success: function( data ) {
+      projectData = data;
+      addMapData();
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log( "ERROR:  " + errorThrown );
+    }
+  });
+
+  var kenyaMap = L.map('main-map').setView([0.0236, 37.9062], 6);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'streets-v9',
     accessToken: 'pk.eyJ1IjoibWFya3JsaSIsImEiOiJjajBzeGptcm0wNGl2Mndqd3dzbWJ5MXdoIn0.6X8tRTfMn_tIZBBORINNfw'
-  }).addTo(mymap);
+  }).addTo(kenyaMap);
+
+  function addMapData() {
+    for (project of projectData["features"]) {
+      if (!project["geometry"]) {
+        return;
+      }
+
+      // coordinates in data are [long, lat]: need to reverse them
+      var coordinates = project["geometry"]["coordinates"].reverse();
+      L.marker(coordinates).addTo(kenyaMap);
+    }
+  }
 });
